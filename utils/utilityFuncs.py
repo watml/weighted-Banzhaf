521cb62bbf69f1b5bdf61ec242a71c2aacfd1c87
Vidas <v3li@uwaterloo.ca>
2023-04-27 14:05:06 +0800

    remove class game_from_date

--- utils/utilityFuncs.py
+++ utils/utilityFuncs.py
@@ -57,117 +57,9 @@ class perturbGame():
         associated_noise = np.prod(self.noise_vec[np.arange(self.num_player),np.int64(subset)])
         return clean_output + associated_noise
 
-class game_from_data():
-    def __init__(self, dataset, *, model_type="logistic", metric="accuracy", game_seed=2023,
-                 lr=0.2, r=1.0,
-                 display=True,
-                 **kwargs):
-        self.game_seed = game_seed
-        self.metric = metric
-        self.criterion = torch.nn.CrossEntropyLoss()
-        self.model_type = model_type
-        self.r = r
-        self.is_set = False
-        self.display = display
-
-        (self.X, self.y), (self.X_val, self.y_val), _ = load_dataset(dataset, display=display, **kwargs)
-        if len(self.y) == 2:
-            self.y = self.y[1]
-        self.num_player = len(self.y)
-        num_feature = self.X.shape[1]
-        num_label = len(np.unique(self.y))
-
-        if model_type == "logistic":
-            self.model = models.LogisticRegression(num_feature, num_label).to(universe.device)
-            self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
-        else:
-            raise NotImplementedError(f"Check {model_type}")
-
-
-        self.X_val = self.X_val.to(universe.device)
-        self.y_val = self.y_val.to(universe.device)
-        self.perm, self.X_train, self.y_train, self.target_prob = None, None, None, None
-        # apply_seed() will initialize perm, X_train, y_train and log_prob under the given seed
 
 
 
-
-    def evaluate(self, subset):
-        idx = subset[self.perm]==1
-        self.train_model(self.X_train[idx], self.y_train[idx])
-        return self.output_score()
-
-    def train_model(self, X, y):
-        torch.manual_seed(self.game_seed)
-        # use model.children(), it will omit some trainable layers deeper in components such as nn.Sequential
-        # model.modules() can solve this issue, but it looks very likely it will bring additional running time
-        # so choose to pay attentino on the structure of model so as to use children()
-        for layer in self.model.children():
-            if hasattr(layer, 'reset_parameters'):
-                layer.reset_parameters()
-        torch.manual_seed(int.from_bytes(os.urandom(8), byteorder="big"))
-        if y.numel():
-            for input, label in zip(X, y):
-                self.optimizer.zero_grad()
-                logit = self.model(input)
-                loss = self.criterion(logit, label)
-                if self.model_type == "logistic":
-                    loss += torch.sum(torch.square(self.model.linear.weight)) * self.r / 2
-                loss.backward()
-                self.optimizer.step()
-
-
-    def output_score(self):
-        self.model.eval()
-        with torch.no_grad():
-            logits = self.model(self.X_val)
-        self.model.train()
-        if self.metric == "accuracy":
-            predict = np.argmax(logits.cpu().numpy(), 1)
-            label = self.y_val.cpu().numpy()
-            score = np.sum(predict==label) / len(label)
-        elif self.metric == "cross_entropy":
-            score = self.criterion(logits, self.y_val).cpu().numpy()
-        elif self.metric == "KL":
-            log_prob = F.log_softmax(logits, dim=1)
-            score = -F.kl_div(log_prob, self.target_prob, reduction="batchmean").cpu().numpy()
-        else:
-            raise NotImplementedError(f"Check {self.metric}")
-        return score
-
-
-    def apply_seed(self):
-        # after setting self.seed=seed, call self.apply_seed to initialize
-        np.random.seed(self.game_seed)
-        self.perm = np.random.permutation(self.num_player)
-        np.random.seed(None)
-        self.X_train = self.X[self.perm].to(universe.device)
-        self.y_train = self.y[self.perm].to(universe.device)
-
-        self.train_model(self.X_train, self.y_train)
-        self.model.eval()
-        with torch.no_grad():
-            logits = self.model(self.X_val)
-        self.model.train()
-
-        self.target_prob = F.softmax(logits, dim=1)
-        predict = np.argmax(logits.cpu().numpy(), 1)
-        label = self.y_val.cpu().numpy()
-        acc = np.sum(predict == label) / len(label)
-        self.is_set = True
-
-        if self.display:
-            print(f"the accuracy trained on full dataset is {acc} given seed {self.game_seed}")
-        return acc, self.target_prob
-
-
-
-
-
-# note multiple instances will be created in parallel running for one experiment setting.
-# the differences are: 1) it won't print anything;
-# 2) it assumes game_seed is given once and for all, i.e, self.apply_seed() is removed;
-# 3) remove metric == "entropy_loss".
 class game_for_parallel():
     def __init__(self, *, dataset, model_type="logistic", metric="accuracy", game_seed=2023, lr=0.2, r=1.0,
                  probability_target=None, **kwargs):
@@ -195,11 +87,8 @@ class game_for_parallel():
 
         self.X_val = self.X_val.to(universe.device)
         self.y_val = self.y_val.to(universe.device)
-        np.random.seed(self.game_seed)
-        self.perm = np.random.permutation(self.num_player)
-        np.random.seed(None)
-        self.X_train = self.X[self.perm].to(universe.device)
-        self.y_train = self.y[self.perm].to(universe.device)
+        self.X_train, self.y_train, self.perm = None, None, None
+        self.apply_seed()
 
         if self.probability_target is None:
             self.train_model(self.X_train, self.y_train)
@@ -257,6 +146,18 @@ class game_for_parallel():
         else:
             raise NotImplementedError(f"Check {self.metric}")
         return score
+
+    def apply_seed(self, game_seed=None):
+        # after setting self.seed=seed, call self.apply_seed to initialize
+        if game_seed is not None:
+            self.game_seed = game_seed
+        np.random.seed(self.game_seed)
+        self.perm = np.random.permutation(self.num_player)
+        np.random.seed(None)
+        self.X_train = self.X[self.perm].to(universe.device)
+        self.y_train = self.y[self.perm].to(universe.device)
+
+
 
 
 
